510b80022e60b3fc7755f5ed9e71527959851b12
ekoxyprince <denniseinstien@gmail.com>
2024-08-08 06:07:41 -0700

    changes

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,8 @@ connectToDb(config.db_url!)
   .then((connected) => {
     server.listen(config.port, () => {
       console.log(`Listening on port ${config.port}`);
+      console.log(`Server running on ${config.server_url}`);
+      console.log(`Client server is on ${config.client_url}`)
     });
     return User.findOne();
   })
