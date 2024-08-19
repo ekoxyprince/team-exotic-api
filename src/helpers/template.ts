4626c96921da9ec80ee1178ce351cd0a01fdb6d6
ekoxyprince <denniseinstien@gmail.com>
2024-08-19 16:01:11 -0700

    changes

--- src/helpers/template.ts
+++ src/helpers/template.ts
@@ -4,7 +4,7 @@ class HtmlTemplate{
     message = (name:string,message:string,subject:string)=>{
         return ` <div style="background: #f5f5f9; width: 100%; height: 100%; ">
         <center>
-            <div style="width: 280px; height: auto;">
+            <div style="width: 100%; height: auto;">
                <img src="https://teamexoticrentals.com/assets/comp-logo-BY9clw1O.png" alt="Goldmarketinc"/>
             </div>
          <div style="width: 92%; height: 200px; background: #fff; padding: 30px 15px; border-radius: 14px;">
